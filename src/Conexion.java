c481c407066d1c106623650dc363acf7c5d907da
Jackson Merma <jmermap@unsa.edu.pe>
2022-06-27 15:29:13 -0500

    clase Conexion implementada (constructor, variables y metodo conectar)

---
+++ src/Conexion.java
@@ -0,0 +1,29 @@
+package src;
+
+import java.sql.DriverManager;
+import java.sql.Connection;
+
+public class Conexion {
+	private String password;
+	private String dataBaseName;
+	private String userName;
+	private Connection con;
+
+	public Conexion(String p, String dbn, String un){
+		this.password = p;
+		this.dataBaseName = dbn;
+		this.userName = un;
+	}
+
+	public void conectar(){
+		System.out.println("Conectando a " + dataBaseName + " ...");
+
+		try{
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			con = DriverManager.getConnection("jdbc:mysql://localhost:3306/" + dataBaseName, userName, password);
+			System.out.println("Base de datos conectada");
+		}catch(Exception e){
+			System.out.println("No se pudo conectar a la base de datos");
+		}
+	}
+}
