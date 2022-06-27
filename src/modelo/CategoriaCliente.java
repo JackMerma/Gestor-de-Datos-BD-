3b15f1c20fd26acf97ace3c8ec07b67b7eb68382
Jackson Merma <jmermap@unsa.edu.pe>
2022-06-27 18:10:22 -0500

    creacion de clase CategoriaCliente

---
+++ src/modelo/CategoriaCliente.java
@@ -0,0 +1,27 @@
+package src.modelo;
+
+public class CategoriaCliente {
+	public int ide;
+	public int limCredito;
+
+	public CategoriaCliente(int ide, int limCredito){
+		this.ide = ide;
+		this.limCredito = limCredito;
+	}
+
+	public void setIde(int ide){
+		this.ide = ide;
+	}
+
+	public void setLimCredito(int limCredito){
+		this.limCredito = limCredito;
+	}
+
+	public int getIde(){
+		return this.ide;
+	}
+
+	public int getLimCredito(){
+		return this.limCredito;
+	}
+}
