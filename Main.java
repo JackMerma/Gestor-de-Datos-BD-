b5f83bfae1bf0f5d2ad95088079100cfe27ecb1e
Jackson Merma <jmermap@unsa.edu.pe>
2022-06-27 16:36:17 -0500

    base para el entorno grafico en Main

--- Main.java
+++ Main.java
@@ -1,10 +1,26 @@
-import java.sql.DriverManager;
-import java.sql.Connection;
-import src.*;
+import src.CargaDatos;
+import src.Conexion;
+
+import java.util.*;
+import java.awt.*;
+import javax.swing.*;
 
 public class Main {
+	
+	public static final String PROJECTNAME = "ZZZZZ";
+	public static JFrame frame;
+
 	public static void main(String [] args){
-		Conexion con = new Conexion ("73123732", "Datos_Personales", "root");
-		con.conectar();
+		frame = new JFrame(PROJECTNAME);
+		frame.setSize(200, 300);
+		frame.setLocationRelativeTo(null);
+
+		generateContent();
+
+		frame.setVisible(true);
+	}
+
+	public static void generateContent(){
+
 	}
 }
