75db51f08f875fe0f1dd0338d53de271b945f1b3
Jackson Merma <jmermap@unsa.edu.pe>
2022-06-27 16:36:53 -0500

    Carga de datos(GUI) base

---
+++ src/CargaDatos.java
@@ -0,0 +1,28 @@
+package src;
+
+// GUI
+import java.util.*;
+import java.awt.*;
+import javax.swing.*;
+
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+// SQL
+import java.sql.DriverManager;
+import java.sql.Connection;
+
+public class CargaDatos {
+
+	public JPanel content;
+	public JPanel a;
+	public JPanel b;
+	public JPanel c;
+
+
+	public JPanel getContent(){
+		return content;
+	}
+}
+
+
