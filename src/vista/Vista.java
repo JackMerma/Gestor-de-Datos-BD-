cea1ce5470877e137eb8348811e211d9053a81e1
Jackson Merma <jmermap@unsa.edu.pe>
2022-06-27 20:48:30 -0500

    variables (paneles, botones), metodos implementados para un grid de 3x1 en el panel principal

--- src/vista/Vista.java
+++ src/vista/Vista.java
@@ -9,14 +9,13 @@ import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 
 public class Vista {
-	public JButton b = new JButton("presioname");
-
 	public JPanel content;
+	private String nombreTabla = "CATEGORÍA CLIENTE";
 
 	// 3 paneles principales
-	public JPanel addPanel;
-	public JPanel viewPanel;
-	public JPanel optionPanel;
+	public JPanel addPanel = new JPanel(new BorderLayout());
+	public JPanel viewPanel = new JPanel(new BorderLayout());
+	public JPanel optionPanel = new JPanel(new BorderLayout());
 
 	// add Panel
 	public JLabel ide = new JLabel();
@@ -24,7 +23,7 @@ public class Vista {
 	public JLabel estaRegis = new JLabel();
 
 	// view Panel
-	
+	//decidir recurso a usar...
 
 	// option Panel
 	public JButton adicionar = new JButton("Adicionar");
@@ -39,10 +38,54 @@ public class Vista {
 
 	public JPanel getContent(){
 		content = new JPanel(new BorderLayout());
+		JPanel centerContent = new JPanel(new GridLayout(3,1));
 
-		content.add(b);
+		// relleno (bordes) en paneles
 
+		createPanelWidth(content);
+		createPanelWidth(addPanel);
+		createPanelWidth(viewPanel);
+		createPanelWidth(optionPanel);
+
+		// creación de contenido en cada panel
+
+		createAddPanelContent();
+		createViewPanelContent();
+		createOptionPanelContent();
+
+		// agregación de los paneles 
+
+		centerContent.add(addPanel);
+		centerContent.add(viewPanel);
+		centerContent.add(optionPanel);
+
+		content.add(centerContent);
+		content.add(new JLabel(this.nombreTabla), BorderLayout.NORTH);//nombre de tabla
 		return content;
+	}
+
+	/*
+	 * Metodos a implementar
+	 */
+
+	public void createAddPanelContent(){
+
+	}
+
+	public void createViewPanelContent(){
+
+	}
+
+	public void createOptionPanelContent(){
+
+	}
+
+
+	public void createPanelWidth(JPanel panel){
+		panel.add(new JLabel(""), BorderLayout.NORTH);
+		panel.add(new JLabel(""), BorderLayout.SOUTH);
+		panel.add(new JLabel("   "), BorderLayout.EAST);
+		panel.add(new JLabel("   "), BorderLayout.WEST);
 	}
 }
 
