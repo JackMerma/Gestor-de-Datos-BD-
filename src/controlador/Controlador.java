24e3f169948d367d9a8c22c66ce3fafcf9bac43c
Jackson Merma <jmermap@unsa.edu.pe>
2022-06-27 19:14:49 -0500

    implementacion del controlador con la vista generada

--- src/controlador/Controlador.java
+++ src/controlador/Controlador.java
@@ -3,18 +3,32 @@ package src.controlador;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 
+import java.util.*;
+import java.awt.*;
+import javax.swing.*;
+
+import src.modelo.CategoriaCliente;
+import src.modelo.CategoriaClienteDAO;
 import src.vista.Vista;
+
+import java.util.ArrayList;
 
 public class Controlador implements ActionListener{
 
-	Vista vista = new Vista();
+	public Vista vista = new Vista();
+	public CategoriaClienteDAO catclidao = new CategoriaClienteDAO();
+	public CategoriaCliente catcli = new CategoriaCliente();
 
 	public Controlador(Vista v){
 		this.vista = v;
+		vista.b.addActionListener(this);
 	}
 
 	public void actionPerformed(ActionEvent e){
-
+		if(e.getSource() == vista.b){
+			ArrayList<CategoriaCliente> dat = catclidao.listar();
+			//no hace nada
+		}
 	}
 }
 
