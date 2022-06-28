cfced0cda5791077d01408b5360434f425d0dcc2
Jackson Merma <jmermap@unsa.edu.pe>
2022-06-27 19:18:00 -0500

    prueba con boton en vista

--- src/vista/Vista.java
+++ src/vista/Vista.java
@@ -15,10 +15,15 @@ public class Vista {
 	//clase DAO
 
 	//botones, frames, etc...
+	public JButton b = new JButton("presioname");
 
 	public JPanel content;
 
 	public JPanel getContent(){
+		content = new JPanel(new BorderLayout());
+
+		content.add(b);
+
 		return content;
 	}
 }
