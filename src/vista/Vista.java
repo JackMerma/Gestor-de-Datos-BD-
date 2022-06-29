5d805211d668e4ba8f058e9982516b3fc8a0dc90
Andy Briyit <ahuamanita@unsa.edu.pe>
2022-06-28 22:30:30 -0500

    division de interfaz

--- src/vista/Vista.java
+++ src/vista/Vista.java
@@ -1,31 +1,80 @@
 package src.vista;
 
-// GUI
-import java.util.*;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+import java.awt.Color;
+
+import java.awt.Dimension;
+import java.awt.Font;
+import java.io.PrintWriter;
 import java.awt.*;
-import javax.swing.*;
 
-import java.awt.event.ActionListener;
-import java.awt.event.ActionEvent;
+public class Vista extends JFrame implements ActionListener {
+	JLabel labelCod, labelDes, labelEst;
+	JTextField txtCod, txtDes, txtEst;
+	JButton adicionar, modificar, eliminar, cancelar, inactivar, reactivar, actualizar, salir;
+	JPanel panelReg, panelTab, panelBot;
 
-//llamar al DAO
+	public Vista() {
+		setTitle("GZZ ESTADO REGISTRO");
+		setLayout(new FlowLayout());
+		setVisible(true);
+		setSize(540, 630);
+		createContens();
+		setLocationRelativeTo(null);
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+	}
 
-public class Vista {
+	public void createContens() {
 
-	//clase DAO
+		// PANEL 1
+		panelReg = new JPanel();
+		panelReg.setPreferredSize(new Dimension(500, 200));
+		panelReg.setBackground(Color.RED);
 
-	//botones, frames, etc...
-	public JButton b = new JButton("presioname");
+		labelCod = new JLabel("Codigo");
+		labelCod.setForeground(Color.WHITE);
+		labelCod.setPreferredSize(new Dimension(500, 35));
+		labelCod.setFont(new Font("Jokerman", 0, 19));
+		panelReg.add(labelCod);
+		add(panelReg);
 
-	public JPanel content;
+		// PANEL 2
+		panelTab = new JPanel();
+		panelTab.setPreferredSize(new Dimension(500, 250));
+		panelTab.setBackground(Color.BLUE);
+		add(panelTab);
 
-	public JPanel getContent(){
-		content = new JPanel(new BorderLayout());
+		// PANEL 3
+		panelBot = new JPanel(new GridLayout(2, 4));
+		panelBot.setPreferredSize(new Dimension(500, 100));
 
-		content.add(b);
+		panelBot.setBackground(Color.DARK_GRAY);
+		adicionar = new JButton("Adicionar");
+		modificar = new JButton("Modificar");
+		eliminar = new JButton("Eliminar");
+		cancelar = new JButton("Cancelar");
+		inactivar = new JButton("Inactivar");
+		reactivar = new JButton("Reactivar");
+		actualizar = new JButton("Actualizar");
+		salir = new JButton("Salir");
+		panelBot.add(adicionar);
+		panelBot.add(modificar);
+		panelBot.add(eliminar);
+		panelBot.add(cancelar);
+		panelBot.add(inactivar);
+		panelBot.add(reactivar);
+		panelBot.add(actualizar);
+		panelBot.add(salir);
 
-		return content;
+		add(panelBot);
+
+	}
+
+	public static void main(String[] args) {
+		Vista newVista = new Vista();
 	}
 }
-
-
