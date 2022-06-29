b70c7cf999a96e4dde4bbd11b06756d9b1bf09c0
Jackson Merma <jmermap@unsa.edu.pe>
2022-06-28 23:14:38 -0500

    implementacion de createAddPanelContent, cambio de tipo de variables para add Panel y cambio de definicion en createPanelWidth

--- src/vista/Vista.java
+++ src/vista/Vista.java
@@ -18,9 +18,9 @@ public class Vista {
 	public JPanel optionPanel = new JPanel(new BorderLayout());
 
 	// add Panel
-	public JLabel ide = new JLabel();
-	public JLabel limCredito = new JLabel();
-	public JLabel estaRegis = new JLabel();
+	public JTextField ide = new JTextField(10);
+	public JTextField limCredito = new JTextField(10);
+	public JTextField estaRegis = new JTextField(1);
 
 	// view Panel
 	//decidir recurso a usar...
@@ -42,7 +42,7 @@ public class Vista {
 
 		// relleno (bordes) en panel
 
-		createPanelWidth(content);
+		createPanelWidth(content, true, true, true, true);
 
 		// creación de contenido en cada panel
 
@@ -66,7 +66,63 @@ public class Vista {
 	 */
 
 	public void createAddPanelContent(){
-		//modifica addPanel
+		addPanel.add(new JLabel("Registro de Categoría Cliente"), BorderLayout.NORTH);
+
+		JPanel addPanelContent = new JPanel(new BorderLayout());
+
+		JPanel addPanelContentContent = new JPanel(new GridLayout(3,1,5,25));
+
+		// ide field
+
+		JPanel idePanel = new JPanel(new GridLayout(1,2));
+		JLabel ideLabel = new JLabel("Ide:");
+		ideLabel.setHorizontalAlignment(JLabel.CENTER);
+
+		idePanel.add(ideLabel);
+
+		JPanel ideButtonPanel= new JPanel(new FlowLayout());
+		//createPanelWidth(ideButtonPanel, true, true, false, false);
+
+		ideButtonPanel.add(ide);
+		idePanel.add(ideButtonPanel);
+
+		// limCredito field
+		
+		JPanel limCreditoPanel = new JPanel(new GridLayout(1,2));
+		JLabel limCreditoLabel = new JLabel("Limite de credito:");
+		limCreditoLabel.setHorizontalAlignment(JLabel.CENTER);
+
+		limCreditoPanel.add(limCreditoLabel);
+
+		JPanel limCreditoButtonPanel = new JPanel(new FlowLayout());
+		//createPanelWidth(limCreditoButtonPanel, true, true, false, false);
+
+		limCreditoButtonPanel.add(limCredito);
+		limCreditoPanel.add(limCreditoButtonPanel);
+
+		// estaRegis field
+
+		JPanel estaRegisPanel = new JPanel(new GridLayout(1,2));
+		JLabel estaRegisLabel = new JLabel("Estado de registro:");
+		estaRegisLabel.setHorizontalAlignment(JLabel.CENTER);
+
+		estaRegisPanel.add(estaRegisLabel);
+
+		JPanel estaRegisButtonPanel = new JPanel(new FlowLayout());
+		//createPanelWidth(estaRegisButtonPanel, true, true, false, false);
+
+		estaRegisButtonPanel.add(estaRegis);
+		estaRegisPanel.add(estaRegisButtonPanel);
+		
+
+		//agregando paneles
+		addPanelContentContent.add(idePanel);
+		addPanelContentContent.add(limCreditoPanel);
+		addPanelContentContent.add(estaRegisPanel);
+
+		createPanelWidth(addPanelContent, true, true, true, true);
+		addPanelContent.add(addPanelContentContent);
+		addPanel.add(addPanelContent);
 	}
 
 	public void createViewPanelContent(){
@@ -78,11 +134,11 @@ public class Vista {
 	}
 
 
-	public void createPanelWidth(JPanel panel){
-		panel.add(new JLabel("\n"), BorderLayout.NORTH);
-		panel.add(new JLabel("\n"), BorderLayout.SOUTH);
-		panel.add(new JLabel("   "), BorderLayout.EAST);
-		panel.add(new JLabel("   "), BorderLayout.WEST);
+	public void createPanelWidth(JPanel panel, boolean n, boolean s, boolean e, boolean w){
+		if(n) panel.add(new JLabel("\n"), BorderLayout.NORTH);
+		if(s) panel.add(new JLabel("\n"), BorderLayout.SOUTH);
+		if(e) panel.add(new JLabel("   "), BorderLayout.EAST);
+		if(w) panel.add(new JLabel("   "), BorderLayout.WEST);
 	}
 }
 
