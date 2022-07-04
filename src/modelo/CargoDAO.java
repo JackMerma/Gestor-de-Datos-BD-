bd80c8af1a90b2c7d62bffaa23e32125347ace36
Jackson Merma <jmermap@unsa.edu.pe>
2022-07-04 15:19:09 -0500

    copia de CategoriaClienteDAO y listado modificado

---
+++ src/modelo/CargoDAO.java
@@ -0,0 +1,95 @@
+package src.modelo;
+
+import java.sql.DriverManager;
+import java.sql.Connection;
+import java.sql.*;
+import java.util.*;
+
+public class CargoDAO {
+
+	public Conexion con = new Conexion();
+	public Cargo cargo = new Cargo();
+	private PreparedStatement ps; 
+	private ResultSet rs;
+	private Connection bd;  
+
+	public ArrayList<Cargo> listar(){
+		ArrayList<Cargo> listCargo = new ArrayList<Cargo>();
+		String sql = "SELECT * FROM `GZZ_CARGO`";
+
+		try{
+			con.conectar();
+			bd = con.getConnection();
+			ps = bd.prepareStatement(sql);
+			rs = ps.executeQuery();
+			// recibimos los resultados
+			while(rs.next()){
+				cargo = new Cargo();
+				cargo.setIde(rs.getInt(1));
+				cargo.setDescripcionCargo(rs.getString(2));
+				cargo.setSueldo(rs.getString(3));
+				cargo.setEstadoRegistro(rs.getString(4).charAt(0));
+				listCargo.add(cargo);
+			}
+			System.out.println("CargoDAO Listar:CORRECTO");
+
+		}catch(Exception e){
+			System.out.println("CargoDAO listar:ERROR");
+			System.err.println(e);
+		}finally{
+			con.desconectar();
+		}
+		
+		return listCargo;
+	}
+
+	// agregar
+	public int add(CategoriaCliente catCli){
+		int state = 0;
+		String sql = "INSERT INTO `GZZ_CATEGORIA_CLIENTE` (`CatCliIde`, `CatCliLimCre`, `CatCliEstReg`) VALUES (?, ?, ?);";
+		try{
+			con.conectar();
+			bd = con.getConnection();
+			ps = bd.prepareStatement(sql);
+			// llenamos los datos
+			ps.setString(1,catCli.getIde()+"");
+			ps.setString(2,catCli.getLimCredito()+"");
+			ps.setString(3, "A");
+			state = ps.executeUpdate();
+			System.out.println("categoriaClienteDao Add: CORRECTO");
+
+		}catch(SQLException e){
+			System.out.println("categoriaClienteDao add: ERROR"); 
+			System.out.println(e);
+		}finally{
+			con.desconectar();
+		}
+		return state;
+	}
+
+	public int modificar(CategoriaCliente catCli){
+		int state = 0;
+		String sql = "UPDATE `GZZ_CATEGORIA_CLIENTE` SET `CatCliLimCre` = ?, `CatCliEstReg` = ? WHERE `GZZ_CATEGORIA_CLIENTE`.`CatCliIde` = ?;";
+		try{
+			con.conectar();
+			bd = con.getConnection();
+			ps = bd.prepareStatement(sql);
+			// llenamos datos
+			ps.setString(1,catCli.getLimCredito()+"");
+			ps.setString(2,catCli.getEstadoRegistro()+"");
+			ps.setString(3,catCli.getIde()+"");
+			state = ps.executeUpdate(); 
+			System.out.println("categoriaClienteDao ModificarInter: CORRECTO");
+		}catch (SQLException e){
+			System.out.println("categoriaClienteDao ModificarInter: ERROR");
+			System.out.println(e);
+		}finally{
+			con.desconectar();
+		}
+
+		return state;
+	}
+
+}
+
+
