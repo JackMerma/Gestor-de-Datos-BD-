da5a32861b36c76fc6498d59ccf8137bbc8854a9
Jackson Merma <jmermap@unsa.edu.pe>
2022-07-04 16:15:01 -0500

    creacion de la clase TipoRepresentanteVenta e implementacion de metodos y atributos

---
+++ src/modelo/TipoRepresentanteVenta.java
@@ -0,0 +1,41 @@
+package src.modelo;
+
+public class TipoRepresentanteVenta {
+	public int ide;
+	public String descTipoRepVenta;
+	public char estadoRegistro;
+
+	public TipoRepresentanteVenta(int ide, String descTipoRepVenta){
+		this.ide = ide;
+		this.descTipoRepVenta = descTipoRepVenta;
+		this.estadoRegistro = 'A';
+	}
+
+	public TipoRepresentanteVenta(){
+		this(-1, "");
+	}
+
+	public void setIde(int ide){
+		this.ide = ide;
+	}
+
+	public void setDescTipoRepVenta(String descTipoRepVenta){
+		this.descTipoRepVenta = descTipoRepVenta;
+	}
+
+	public void setEstadoRegistro(char s){
+		this.estadoRegistro = s;
+	}
+
+	public int getIde(){
+		return this.ide;
+	}
+
+	public String getDescTipoRepVenta(){
+		return this.descTipoRepVenta;
+	}
+
+	public char getEstadoRegistro(){
+		return this.estadoRegistro;
+	}
+}
