94fa58e095bbfb589f077a66528cb28c42672322
PaulAlexander19 <pluquec@unsa.edu.pe>
2022-07-05 22:53:31 -0500

    agregando unos botones para mistrar cada una de las tablas referenciales

--- Main.java
+++ Main.java
@@ -10,16 +10,34 @@ public class Main {
 	
 	public static final String PROJECTNAME = "EMPRESA DE DISTRIBUCION";
 	public static JFrame frame;
+	public static JFrame frameInitial;
 
 	public static void main(String [] args){
+
+		// creando una interface adicional para probar las demas
+		frameInitial = new JFrame("INICIO TABLAS REFERENCIALES");
+		frameInitial.setSize(700, 200);
+		frameInitial.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frameInitial.setLocationRelativeTo(null);
+
+		frameInitial.setLayout(new BorderLayout()); 
+		JLabel label = new JLabel("Tablas Referencioales", SwingConstants.CENTER);
+		frameInitial.add(label, BorderLayout.NORTH);
+
+		// agregando los otros paneles
+		addButtonsTableReference();
+		
+		frameInitial.setVisible(true);
+		
+		/*
 		frame = new JFrame(PROJECTNAME);
 		frame.setSize(500, 600);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setLocationRelativeTo(null);
 
 		generateContent();
-
 		frame.setVisible(true);
+		*/
 		//probando la coneccion
 		// CategoriaClienteDAO test = new CategoriaClienteDAO();
 		// CategoriaCliente catCli = new CategoriaCliente(2,2000);// mejorar esta parte, se duplica la clave
@@ -28,6 +46,27 @@ public class Main {
 		// //test.add(catCli);
 		// test.listar();
 
+	}
+
+	public static void addButtonsTableReference(){
+		JPanel panel = new JPanel();
+
+		// creando el panel para los botones internos
+		panel.setLayout(new GridLayout(2,2,5,10));	
+		
+		panel.add(addPanelButton("Cargo"));
+		panel.add(addPanelButton("Categoria Cliente"));
+		panel.add(addPanelButton("Rol Registro"));
+		panel.add(addPanelButton("Tipo Representante Venta"));
+		
+		frameInitial.add(panel, BorderLayout.CENTER);
+	}
+
+	public static JPanel addPanelButton(String titleTableReference){
+		JPanel panelInter = new JPanel();
+		panelInter.add(new JLabel(titleTableReference));
+		panelInter.add(new JPanel().add(new JButton("Abrir Interfaz")));
+		return panelInter;
 	}
 
 	public static void generateContent(){
