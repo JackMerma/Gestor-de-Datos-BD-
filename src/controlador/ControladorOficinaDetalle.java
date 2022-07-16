f7f48d1de35ff925db0e5261dce485b939b8968d
PaulAlexander19 <pluquec@unsa.edu.pe>
2022-07-15 19:33:08 -0500

    antes que se cambie a genaral el controlador la parte de actionPerformed

--- src/controlador/ControladorOficinaDetalle.java
+++ src/controlador/ControladorOficinaDetalle.java
@@ -9,25 +9,29 @@ import javax.swing.*;
 
 import src.modelo.Cargo.Cargo;
 import src.modelo.Cargo.CargoDAO;
+import src.modelo.OficinaDetalle.OficinaDetalle;
+import src.modelo.OficinaDetalle.OficinaDetalleDAO;
 import src.vista.VistaCargo;
+import src.vista.VistaOficinaDetalle;
+
 import javax.swing.JOptionPane;
 import javax.swing.table.DefaultTableCellRenderer;
 import javax.swing.table.DefaultTableModel;
 
 import java.util.ArrayList;
 
-public class ControladorOficinaDetalle implements ActionListener{
+public class ControladorOficinaDetalle implements ActionListener {
 
-	public VistaCargo vista = new VistaCargo();
-	public CargoDAO cargodao = new CargoDAO();
-	public Cargo cargo = new Cargo();
+	public VistaOficinaDetalle vista = new VistaOficinaDetalle();
+	public OficinaDetalleDAO oficinaDetalleDAO = new OficinaDetalleDAO();
+	public OficinaDetalle ofiDetalle = new OficinaDetalle();
 	DefaultTableModel modelo = new DefaultTableModel();
 
 	private int CarFlaAct = 0;
-	private int action; // 1: agregar, 
+	private int action; // 1: agregar,
 
 	// constructor
-	public ControladorOficinaDetalle(VistaCargo v){
+	public ControladorOficinaDetalle(VistaOficinaDetalle v) {
 		this.vista = v;
 		vista.adicionar.addActionListener(this);
 		vista.modificar.addActionListener(this);
@@ -40,26 +44,43 @@ public class ControladorOficinaDetalle implements ActionListener{
 		listar(vista.tabla);
 	}
 
-	// metodo que agrera a una nueva categoria
-	private void agregar(){
-
-		// Obtenemos datos
+	// metodo auxiliar para obtener un objeto con los datos de la vista
+	private OficinaDetalle getOficinaDetalle() {
+		OficinaDetalle ofide = new OficinaDetalle();
 		String ide = vista.ide.getText();
-		String desc = vista.descripcion.getText();
-		String sueldo = vista.sueldo.getText();
-		String esta = vista.estaRegis.getText();
+		String ciudad = vista.ciudad.getText();
+		String region = vista.region.getText();
+		String direccion = vista.direccion.getText();
+		String empleado = vista.empleado.getText();
+		String objetivo = vista.objetivo.getText();
+		String ventas = vista.ventas.getText();
+		String estaRegis = vista.estaRegis.getText();
 
 		// Creamos un objeto
-		cargo.setIde(Integer.parseInt(ide));
-		cargo.setDescripcionCargo(desc);
-		cargo.setSueldo(Integer.parseInt(sueldo));
-		cargo.setEstadoRegistro(esta.charAt(0));
+		ofide.setOfiIde(Integer.parseInt(ide));
+		ofide.setOfiCiu(ciudad);
+		ofide.setOfiReg(region);
+		ofide.setOfiDir(direccion);
+		ofide.setOfiEmp(empleado);
+		ofide.setOfiObj(Integer.parseInt(objetivo));
+		ofide.setOfiVen(ventas);
+		ofide.setOfiEstReg(estaRegis);
+
+		return ofide;
+	}
+
+	// metodo que agrera a una nueva categoria
+	private void agregar() {
+
+		// Obtenemos datos
+
+		ofiDetalle = getOficinaDetalle();
 
 		// limpiamos la tabla anterior
 		limpiarTabla();
 
-		// agregamos 
-		int n = cargodao.add(cargo);
+		// agregamos
+		int n = oficinaDetalleDAO.add(ofiDetalle);
 
 		// verificamor resultado
 		if (n == 1) {
@@ -76,24 +97,18 @@ public class ControladorOficinaDetalle implements ActionListener{
 	}
 
 	// Para modificar una categoria
-	private void modificar(){
-		// Obtenemos datos
-		String ide = vista.ide.getText();
-		String desc = vista.descripcion.getText();
-		String sueldo = vista.sueldo.getText();
-		String esta = vista.estaRegis.getText();
+	private void modificar() {
 
-		// Creamos un objeto
-		cargo.setIde(Integer.parseInt(ide));
-		cargo.setDescripcionCargo(desc);
-		cargo.setSueldo(Integer.parseInt(sueldo));
-		cargo.setEstadoRegistro(esta.charAt(0));
+		// Obtenemos datos
+
+		ofiDetalle = getOficinaDetalle();
 
 		// limpiamos la tabla anterior
 		limpiarTabla();
 
-		// agregamos 
-		int n = cargodao.modificar(cargo);
+		// Modificamos
+		int n = oficinaDetalleDAO.modificar(ofiDetalle);
+
 
 		// verificamor resultado
 		if (n == 1) {
@@ -110,24 +125,16 @@ public class ControladorOficinaDetalle implements ActionListener{
 	}
 
 	// Para eliminar una categoria
-	private void eliminar(){
+	private void eliminar() {
 		// Obtenemos datos
-		String ide = vista.ide.getText();
-		String desc = vista.descripcion.getText();
-		String sueldo = vista.sueldo.getText();
-		String esta = vista.estaRegis.getText();
 
-		// Creamos un objeto
-		cargo.setIde(Integer.parseInt(ide));
-		cargo.setDescripcionCargo(desc);
-		cargo.setSueldo(Integer.parseInt(sueldo));
-		cargo.setEstadoRegistro(esta.charAt(0));
+		ofiDetalle = getOficinaDetalle();
 
 		// limpiamos la tabla anterior
 		limpiarTabla();
 
-		// agregamos 
-		int n = cargodao.modificar(cargo);
+		// Modificamos
+		int n = oficinaDetalleDAO.modificar(ofiDetalle);
 
 		// verificamor resultado
 		if (n == 1) {
@@ -144,24 +151,16 @@ public class ControladorOficinaDetalle implements ActionListener{
 	}
 
 	// Para inavilitar una categoria
-	private void inavilitar(){
+	private void inavilitar() {
 		// Obtenemos datos
-		String ide = vista.ide.getText();
-		String desc = vista.descripcion.getText();
-		String sueldo = vista.sueldo.getText();
-		String esta = vista.estaRegis.getText();
 
-		// Creamos un objeto
-		cargo.setIde(Integer.parseInt(ide));
-		cargo.setDescripcionCargo(desc);
-		cargo.setSueldo(Integer.parseInt(sueldo));
-		cargo.setEstadoRegistro(esta.charAt(0));
+		ofiDetalle = getOficinaDetalle();
 
 		// limpiamos la tabla anterior
 		limpiarTabla();
 
-		// agregamos 
-		int n = cargodao.modificar(cargo);
+		// Modificamos
+		int n = oficinaDetalleDAO.modificar(ofiDetalle);
 
 		// verificamor resultado
 		if (n == 1) {
@@ -177,26 +176,18 @@ public class ControladorOficinaDetalle implements ActionListener{
 		listar(vista.tabla);
 	}
 
-
 	// Para reactivar una categoria
-	private void reactivar(){
+	private void reactivar() {
 		// Obtenemos datos
-		String ide = vista.ide.getText();
-		String desc = vista.descripcion.getText();
-		String sueldo = vista.sueldo.getText();
-		String esta = vista.estaRegis.getText();
 
-		// Creamos un objeto
-		cargo.setIde(Integer.parseInt(ide));
-		cargo.setDescripcionCargo(desc);
-		cargo.setSueldo(Integer.parseInt(sueldo));
-		cargo.setEstadoRegistro(esta.charAt(0));
+		ofiDetalle = getOficinaDetalle();
 
 		// limpiamos la tabla anterior
 		limpiarTabla();
 
-		// agregamos 
-		int n = cargodao.modificar(cargo);
+		// Modificamos
+		int n = oficinaDetalleDAO.modificar(ofiDetalle);
+
 
 		// verificamor resultado
 		if (n == 1) {
@@ -210,7 +201,7 @@ public class ControladorOficinaDetalle implements ActionListener{
 
 		// listamor nueva tabla
 		listar(vista.tabla);
-	}	
+	}
 
 	// lista los nuevos datos
 	public void listar(JTable tabla) {
@@ -218,32 +209,35 @@ public class ControladorOficinaDetalle implements ActionListener{
 		modelo = (DefaultTableModel) tabla.getModel();
 		tabla.setModel(modelo);
 
-		ArrayList<Cargo> lista = cargodao.listar();
+		ArrayList<OficinaDetalle> lista = oficinaDetalleDAO.listar();
 		Object[] objeto = new Object[4];
 
 		for (int i = 0; i < lista.size(); i++) {
-			objeto[0] = lista.get(i).getIde();
-			objeto[1] = lista.get(i).getDescripcionCargo();
-			objeto[2] = lista.get(i).getSueldo();
-			objeto[3] = lista.get(i).getEstadoRegistro();
+			objeto[0] = lista.get(i).getOfiIde();
+			objeto[1] = lista.get(i).getOfiCiu();
+			objeto[2] = lista.get(i).getOfiReg();
+			objeto[3] = lista.get(i).getOfiDir();
+			objeto[4] = lista.get(i).getOfiEmp();
+			objeto[5] = lista.get(i).getOfiObj();
+			objeto[6] = lista.get(i).getOfiVen();
+			objeto[7] = lista.get(i).getOfiEstReg();
 			modelo.addRow(objeto);
 		}
 	}
 
+	
 
-
-
-	public void actionPerformed(ActionEvent e){
-		if(e.getSource() == vista.adicionar){
+	public void actionPerformed(ActionEvent e) {
+		if (e.getSource() == vista.adicionar) {
 			System.out.println("Apreto: adicionar");
-			//limpiar();
+			// limpiar();
 			vista.estaRegis.setText("A");
 			vista.estaRegis.setEditable(false);
 
 			CarFlaAct = 1;
-			action=1;
+			action = 1;
 
-		}else if(e.getSource() == vista.modificar){
+		} else if (e.getSource() == vista.modificar) {
 			System.out.println("Apreto: modificar");
 			int fila = vista.tabla.getSelectedRow();
 
@@ -269,7 +263,7 @@ public class ControladorOficinaDetalle implements ActionListener{
 
 			}
 
-		}else if(e.getSource() == vista.eliminar){
+		} else if (e.getSource() == vista.eliminar) {
 			System.out.println("Apreto: eliminar");
 
 			int fila = vista.tabla.getSelectedRow();
@@ -295,12 +289,12 @@ public class ControladorOficinaDetalle implements ActionListener{
 				action = 3;
 
 			}
-		}else if(e.getSource() == vista.cancelar){
+		} else if (e.getSource() == vista.cancelar) {
 			System.out.println("Apreto: cancelar");
-			CarFlaAct=0;
+			CarFlaAct = 0;
 			limpiar();
 
-		}else if(e.getSource() == vista.inactivar){
+		} else if (e.getSource() == vista.inactivar) {
 			System.out.println("Apreto: inactivar");
 
 			int fila = vista.tabla.getSelectedRow();
@@ -326,7 +320,7 @@ public class ControladorOficinaDetalle implements ActionListener{
 				action = 4;
 
 			}
-		}else if(e.getSource() == vista.reactivar){
+		} else if (e.getSource() == vista.reactivar) {
 			System.out.println("Apreto: reactivar");
 
 			int fila = vista.tabla.getSelectedRow();
@@ -352,39 +346,40 @@ public class ControladorOficinaDetalle implements ActionListener{
 				action = 5;
 
 			}
-		}else if(e.getSource() == vista.actualizar){
+		} else if (e.getSource() == vista.actualizar) {
 			System.out.println("Apreto: actualizar");
-			if(CarFlaAct==1){
+			if (CarFlaAct == 1) {
 				if (JOptionPane.showConfirmDialog(null, "Está seguro que desea realizar esta acción", "WARNING",
-							JOptionPane.YES_NO_OPTION) == JOptionPane.YES_OPTION) {
+						JOptionPane.YES_NO_OPTION) == JOptionPane.YES_OPTION) {
 					System.out.println("Afecta a la bd");
 
-					if(action==1){ //Agregar
+					if (action == 1) { // Agregar
 						agregar();
-					}else if(action == 2){ //Modificar
+					} else if (action == 2) { // Modificar
 						modificar();
-					}else if(action == 3){ //Eliminar
+					} else if (action == 3) { // Eliminar
 						eliminar();
-					}else if(action == 4){ //Inavilitar
+					} else if (action == 4) { // Inavilitar
 						inavilitar();
-					}else if(action == 5){ //Reactivar
+					} else if (action == 5) { // Reactivar
 						reactivar();
 					}
 
-					CarFlaAct=0;	
-				}				
+					CarFlaAct = 0;
+				}
 
-			}else{
-				JOptionPane.showMessageDialog(null, "Opción no es válida","Alerta",JOptionPane.WARNING_MESSAGE);
+			} else {
+				JOptionPane.showMessageDialog(null, "Opción no es válida", "Alerta", JOptionPane.WARNING_MESSAGE);
 			}
-		}else if(e.getSource() == vista.salir){
+		} else if (e.getSource() == vista.salir) {
 			System.out.println("Apreto: salir ");
 			System.exit(0);
 
 		}
 	}
+
 	// limpia la entrada de texto
-	private void limpiar(){
+	private void limpiar() {
 		vista.ide.setText("");
 		vista.descripcion.setText("");
 		vista.sueldo.setText("");
