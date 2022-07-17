2ecc595896532a742eef0b159e954d321808c252
PaulAlexander19 <pluquec@unsa.edu.pe>
2022-07-17 10:09:55 -0500

    agregando cambias en la configuracion para usar ngrok

--- src/modelo/Configuracion.java
+++ src/modelo/Configuracion.java
@@ -2,7 +2,7 @@ package src.modelo;
 
 public interface Configuracion {
     // String HOST = "6.tcp.ngrok.io";
-    String HOST = "localhost";
-    String PORT = "3306";
+    String HOST = "0.tcp.sa.ngrok.io";
+    String PORT = "14304";
 
 }
